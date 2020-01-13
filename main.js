1e8e3a8a7f9dc3c9408803e17412ecb430987301
TiPrajapati <59812961+TiPrajapati@users.noreply.github.com>
2020-01-12 21:59:40 -0500

    Add aboutMe File

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function(){
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+
+    });
+
+    $('.top-nav .nav-link').on('click', function(){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+
+    });
+
+    $('nav a[href*="#"]').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+
+    });
+
+    $('#up').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    })
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
